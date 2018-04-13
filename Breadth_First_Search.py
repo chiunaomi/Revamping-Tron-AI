0460f98676a94644a2541d74e11bbdfa19ddedc2
AlexFrye <alex.frye@students.olin.edu>
2018-04-13 11:48:50 -0400

    adding placeholder grid (still buggy), bfs should work with grids now

--- Breadth_First_Search.py
+++ Breadth_First_Search.py
@@ -9,8 +9,10 @@ Takes: gird state (with no-go areas)
 
 make implementaiton of breadth first, don't worry about adding to game
 """
+import collections
 
-"""creates a simple grid, replace with game"""
+"""
+#creates a simple grid, replace with game
 class SimpleGraph:
     def __init__(self):
         self.edges = {}
@@ -27,7 +29,6 @@ example_graph.edges = {
     'E': ['B']
 }
 
-import collections
 
 class Queue:
     def __init__(self):
@@ -55,56 +56,40 @@ def bfs(graph, start):
                 frontier.put(next)
                 visited[next] = True
 bfs(example_graph, 'A')
+"""
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
 
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, x):
+        self.elements.append(x)
+
+    def get(self):
+        return self.elements.popleft()
 #######################################
 """basically what we already have - a grid with walls"""
-def from_id_width(id, width):
-    return (id % width, id // width)
-
-def draw_tile(graph, id, style, width):
-    r = "."
-    if 'number' in style and id in style['number']: r = "%d" % style['number'][id]
-    if 'point_to' in style and style['point_to'].get(id, None) is not None:
-        (x1, y1) = id
-        (x2, y2) = style['point_to'][id]
-        if x2 == x1 + 1: r = ">"
-        if x2 == x1 - 1: r = "<"
-        if y2 == y1 + 1: r = "v"
-        if y2 == y1 - 1: r = "^"
-    if 'start' in style and id == style['start']: r = "A"
-    if 'goal' in style and id == style['goal']: r = "Z"
-    if 'path' in style and id in style['path']: r = "@"
-    if id in graph.walls: r = "#" * width
-    return r
-
-def draw_grid(graph, width=2, **style):
-    for y in range(graph.height):
-        for x in range(graph.width):
-            print("%%-%ds" % width % draw_tile(graph, (x, y), style, width), end="")
-        print()
-
-class SquareGrid:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.walls = []
-
-    def in_bounds(self, id):
-        (x, y) = id
-        return 0 <= x < self.width and 0 <= y < self.height
-
-    def passable(self, id):
-        return id not in self.walls
-
-    def neighbors(self, id):
-        (x, y) = id
-        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
-        if (x + y) % 2 == 0: results.reverse() # aesthetics
-        results = filter(self.in_bounds, results)
-        results = filter(self.passable, results)
-        return results
-
-myGrid = SquareGrid(30,30)
-myGrid.walls =[(15,15), (15,16),(15,17),(15,18),(20,3),(20,4),(21,3),(21,4)]
+from DFS_Placeholder_Grid import *
+myGrid = SquareGrid(30,20)
+myGrid.walls = DIAGRAM1_WALLS
 """makes a list of walls - same as areas already been to in tron"""
 draw_grid(myGrid)
+
+def bfs_grid(graph, start):
+    #actual alogrithm - send gird, gamestate, and player's location)
+    frontier = Queue()
+    frontier.put(start)
+    came_from = {}
+    came_from[start] = True
+
+    while not frontier.empty():
+        current = frontier.get() #gets next 'frontier'(verticie) from the queue
+        for next in graph.neighbors(current):
+            if next not in came_from:
+                frontier.put(next)
+                came_from[next] = True
+    return came_from
+parents = bfs_grid(myGrid, (8,7))
+draw_grid(myGrid, width = 2, point_to = parents, start = (8,7))
