7e6ce97ca14634551805ebe9640b7698feace08c
Naomi <orangenaomic@gmail.com>
2018-05-01 23:05:49 -0400

    Added controls for up to four players and crash recognition in progress

--- tronmodel.py
+++ tronmodel.py
@@ -20,12 +20,13 @@ class TronModel(object):
         self.width = width
         self.height = height
         self.cell_length = cell_length
-        self.cell_lst = []
-        self.player_paths = []
-        self.player_colors = ((255,140,0),(0,250,0),(0,150,150),(255,0,0))
+        self.cell_lst = set()
+        self.player_paths = set()
+        self.player_colors = [(255,140,0),(0,250,0),(0,150,150),(255,0,0)]
+        self.color_strings = ["Orange","Green","Blue","Red"]
         for i in range(self.height//cell_length):
             for j in range(self.width//cell_length):
-                self.cell_lst.append(Cell(self.screen,(i*self.cell_length,j*self.cell_length),cell_length))
+                self.cell_lst.add(Cell(self.screen,(i*self.cell_length,j*self.cell_length),cell_length))
         self.game_over = False
         self.end_start = False
         self.end_setup = False
@@ -33,23 +34,27 @@ class TronModel(object):
     def init_players(self):
         "Initiates number of players specified by user input"
         if self.num_players == 1:
-            self.player1 = Player(self.screen,10,(self.width/2+100),(self.height/2),"r",self.player_colors[0])
+            self.player1 = Player(self.screen,10,(self.width/2-100),(self.height/2),"l",self.color_strings[0],self.player_colors[0])
             self.players = [self.player1]
+            print(self.players)
         if self.num_players == 2:
-            self.player1 = Player(self.screen,10,(self.width/2+100),(self.height/2),"r",self.player_colors[0])
-            self.player2 = Player(self.screen,10,(self.width/2-100),(self.height/2),"l",self.player_colors[1])
+            self.player1 = Player(self.screen,10,(self.width/2-100),(self.height/2),"l",self.color_strings[0],self.player_colors[0])
+            self.player2 = Player(self.screen,10,(self.width/2+100),(self.height/2),"r",self.color_strings[1],self.player_colors[1])
             self.players = [self.player1,self.player2]
+            print(self.players)
         if self.num_players == 3:
-            self.player1 = Player(self.screen,10,(self.width/2+100),(self.height/2-50),"r",self.player_colors[0])
-            self.player2 = Player(self.screen,10,(self.width/2-100),(self.height/2-50),"l",self.player_colors[1])
-            self.player3 = Player(self.screen,10,(self.width/2+100),(self.height/2+50),"r",self.player_colors[2])
+            self.player1 = Player(self.screen,10,(self.width/2-100),(self.height/2-50),"l",self.color_strings[0],self.player_colors[0])
+            self.player2 = Player(self.screen,10,(self.width/2+100),(self.height/2-50),"r",self.color_strings[1],self.player_colors[1])
+            self.player3 = Player(self.screen,10,(self.width/2-100),(self.height/2+50),"l",self.color_strings[2],self.player_colors[2])
             self.players = [self.player1,self.player2,self.player3]
+            print(self.players)
         if self.num_players == 4:
-            self.player1 = Player(self.screen,10,(self.width/2+100),(self.height/2-50),"r",self.player_colors[0])
-            self.player2 = Player(self.screen,10,(self.width/2-100),(self.height/2-50),"l",self.player_colors[1])
-            self.player3 = Player(self.screen,10,(self.width/2+100),(self.height/2+50),"r",self.player_colors[2])
-            self.player4 = Player(self.screen,10,(self.width/2-100),(self.height/2+50),"l",self.player_colors[3])
+            self.player1 = Player(self.screen,10,(self.width/2-100),(self.height/2-50),"l",self.color_strings[0],self.player_colors[0])
+            self.player2 = Player(self.screen,10,(self.width/2+100),(self.height/2-50),"r",self.color_strings[1],self.player_colors[1])
+            self.player3 = Player(self.screen,10,(self.width/2-100),(self.height/2+50),"l",self.color_strings[2],self.player_colors[2])
+            self.player4 = Player(self.screen,10,(self.width/2+100),(self.height/2+50),"r",self.color_strings[3],self.player_colors[3])
             self.players = [self.player1,self.player2,self.player3,self.player4]
+            print(self.players)
 
     def _draw_players(self):
         """Calls the player objects' draw functions"""
@@ -59,46 +64,38 @@ class TronModel(object):
     def in_cell(self):
         """Loops through cell_lst to find the cell whose xrange contains player.x
         and whose yrange contains player.y, and sets the player location to be within that cell."""
-        for cell in self.cell_lst:
-            if self.player1.x in cell.xrange and self.player1.y in cell.yrange:
-                self.player1.current_cell = cell
-                break
-        for cell in self.cell_lst:
-            if self.player2.x in cell.xrange and self.player2.y in cell.yrange:
-                self.player2.current_cell = cell
-                break
+        for player in self.players:
+            for cell in self.cell_lst:
+                if player.x in cell.xrange and player.y in cell.yrange:
+                    player.current_cell = cell
+                    break
 
     def update(self):
         """Checks for new inputs and updates the game model."""
+        player_num = 0
         for player in self.players:
+            player_num += 1
             player.update()
-        if self.player1.crash():
-            self.end_game("GREEN ",(0,250,0))
-        if self.player2.crash():
-            self.end_game("ORANGE ",(255,140,0))
+            player.last_seen = player.current_cell
 
-        last_seen_p1 = self.player1.current_cell
-        last_seen_p2 = self.player2.current_cell
-        # Saving the player locations before updating in order to test to see if the players
-        # have entered a new cell.
         self.in_cell()
-        if self.player1.current_cell != last_seen_p1:
-            self.player_paths.append(last_seen_p1)
-        if self.player2.current_cell != last_seen_p2:
-            self.player_paths.append(last_seen_p2)
+        for player in self.players:
+            if player.current_cell != player.last_seen:
+                self.player_paths.add(player.last_seen)
         #If the player has left a cell and moved into another, the vacated cell is
         #added to the list of cells that have been hit
-
-        if self.player1.current_cell in self.player_paths:
-            self.end_game("GREEN ",(0,250,0))
-        if self.player2.current_cell in self.player_paths:
-            self.end_game("ORANGE ",(255,140,0))
+            if player.current_cell in self.player_paths:
+                self.players.remove(player)
+                print(self.players)
+                player.alive = False
+        if len(self.players) == 1:
+            self.end_game(self.players[0].name,self.players[0].color)
 
     def end_game(self,player,color):
         """Contains end game protocol and end game display."""
         black = (0, 0, 0)
         font = pygame.font.Font(os.path.join(os.path.dirname(os.path.realpath(__file__)),'TRON.TTF'), 25)
-        label1= font.render(player + "WINS!", 1, color)
+        label1= font.render(player + " WINS!", 1, color)
         label2 = font.render("Press Space to Restart", 1, (255,255,255))
         self.screen.fill(black)
         self.screen.blit(label1,(185,100))
